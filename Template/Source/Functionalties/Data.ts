cda7ee2f0a8ea0ceb4e835a00ee45d7c8103984a
yamisukii <stefan.merdian@hs-furtwangen.de>
2023-07-25 08:45:06 +0200

    changed url

--- Template/Source/Functionalties/Data.ts
+++ Template/Source/Functionalties/Data.ts
@@ -5,7 +5,7 @@ namespace VisualNovel {
   export let transitions = {
     water: {
       duration: 1.5,
-      alpha: "/Template/FreeTransitions/Others/002.jpg",
+      alpha: "./FreeTransitions/Others/002.jpg",
       edge: 1
     },
     thinking: {
@@ -31,27 +31,27 @@ namespace VisualNovel {
   };
 
   export let soundKaIntro = {
-    traffic1: "/Template/Audio/Kapitel1/Traffic_1.wav",
-    traffic2: "/Template/Audio/Kapitel1/Traffic_2.wav",
-    nachdenklich: "/Template/Audio/Kapitel1/Nachdenkliche_Musik.wav",
-    dramatic1: "/Template/Audio/Kapitel1/Dramatic_1.wav",
-    dramatic2: "/Template/Audio/Kapitel1/Dramatic_2.wav",
-    fastPiano: "/Template/Audio/fastPiano.mp3",
-    sadPaino: "/Template/Audio/sadPiano.mp3",
+    traffic1: "./Audio/Kapitel1/Traffic_1.wav",
+    traffic2: "./Audio/Kapitel1/Traffic_2.wav",
+    nachdenklich: "./Audio/Kapitel1/Nachdenkliche_Musik.wav",
+    dramatic1: "./Audio/Kapitel1/Dramatic_1.wav",
+    dramatic2: "./Audio/Kapitel1/Dramatic_2.wav",
+    fastPiano: "./Audio/fastPiano.mp3",
+    sadPaino: "./Audio/sadPiano.mp3",
   };
   export let soundKaRenji = {
-    deppresiv: "/Template/Audio/Kapitel2/depressiv.mp3",
+    deppresiv: "./Audio/Kapitel2/depressiv.mp3",
   };
   export let soundKaAyumi = {
-    correct: "/Template/Audio/Kapitel3/correct.wav",
-    wrong: "/Template/Audio/Kapitel3/wromg.wav",
-    water: "/Template/Audio/Kapitel3/water.wav",
-    empty: "/Template/Audio/Kapitel3/Empty_Hall2.flac",
+    correct: "./Audio/Kapitel3/correct.wav",
+    wrong: "./Audio/Kapitel3/wromg.wav",
+    water: "./Audio/Kapitel3/water.wav",
+    empty: "./Audio/Kapitel3/Empty_Hall2.flac",
   };
   export let soundKaATakeshi = {
-    bar: "/Template/Audio/Kapitel4/Bar.wav",
-    dark1: "/Template/Audio/Kapitel4/Dark_Athmo_1.wav",
-    dark2: "/Template/Audio/Kapitel4/Dark_Athmo_2.wav",
+    bar: "./Audio/Kapitel4/Bar.wav",
+    dark1: "./Audio/Kapitel4/Dark_Athmo_1.wav",
+    dark2: "./Audio/Kapitel4/Dark_Athmo_2.wav",
 
   };
 
@@ -59,72 +59,72 @@ namespace VisualNovel {
 
     heaven: {
       name: "endScene1",
-      background: "/Template/Images/Backgrounds/End/Heaven.png"
+      background: "./Images/Backgrounds/End/Heaven.png"
     },
     endScene1: {
       name: "endScene1",
-      background: "/Template/Images/Backgrounds/BridgeScene/FallScene/IWillRemeberYou.png"
+      background: "./Images/Backgrounds/BridgeScene/FallScene/IWillRemeberYou.png"
     },
     bar: {
       name: "rainyDistrict",
-      background: "/Template/Images/Backgrounds/RevengeCase/Bar.png"
+      background: "./Images/Backgrounds/RevengeCase/Bar.png"
     },
     rainyDistrict: {
       name: "rainyDistrict",
-      background: "/Template/Images/Backgrounds/RevengeCase/rainyDistrict.png"
+      background: "./Images/Backgrounds/RevengeCase/rainyDistrict.png"
     },
     swimmingHall: {
       name: "swimminHall",
-      background: "/Template/Images/Backgrounds/swimmingHall/swimmingHall.png"
+      background: "./Images/Backgrounds/swimmingHall/swimmingHall.png"
     },
     renjisWorld1: {
       name: "renjisWorld1",
-      background: "/Template/Images/Backgrounds/RenjisWorld/RenjisWorld1.png"
+      background: "./Images/Backgrounds/RenjisWorld/RenjisWorld1.png"
     },
     renjisWorld2: {
       name: "renjisWorld2",
-      background: "/Template/Images/Backgrounds/RenjisWorld/RenjisWorld2.png"
+      background: "./Images/Backgrounds/RenjisWorld/RenjisWorld2.png"
     },
     memorie1: {
       name: "Memorie1",
-      background: "/Template/Images/Backgrounds/BridgeScene/Feelings/Memories1.png"
+      background: "./Images/Backgrounds/BridgeScene/Feelings/Memories1.png"
     },
     memorie2: {
       name: "Memorie2",
-      background: "/Template/Images/Backgrounds/BridgeScene/Feelings/Memories2.png"
+      background: "./Images/Backgrounds/BridgeScene/Feelings/Memories2.png"
     },
     memorie3: {
       name: "Memorie3",
-      background: "/Template/Images/Backgrounds/BridgeScene/Feelings/Memories3.png"
+      background: "./Images/Backgrounds/BridgeScene/Feelings/Memories3.png"
     },
     fall2: {
       name: "FallingDown1",
-      background: "/Template/Images/Backgrounds/BridgeScene/FallScene/Falling2.png"
+      background: "./Images/Backgrounds/BridgeScene/FallScene/Falling2.png"
     },
     fall1: {
       name: "FallingDown1",
-      background: "/Template/Images/Backgrounds/BridgeScene/FallScene/Falling.png"
+      background: "./Images/Backgrounds/BridgeScene/FallScene/Falling.png"
     },
     tokyoBridge: {
       name: "TokyoBridge",
-      background: "/Template/Images/Backgrounds/BridgeScene/BridgeGirl.png"
+      background: "./Images/Backgrounds/BridgeScene/BridgeGirl.png"
     },
     tokyoWalkingHome: {
       name: "TokyoAtEvenning",
-      background: "/Template/Images/Backgrounds/Tokyo/WalkingHome.png"
+      background: "./Images/Backgrounds/Tokyo/WalkingHome.png"
     },
 
     purgatory: {
       name: "DecisionWorld",
-      background: "/Template/Images/BG/DescisionWorld.jpg"
+      background: "./Images/BG/DescisionWorld.jpg"
     },
     alleyEv: {
       name: "alleyEvening",
-      background: "/Template/Images/BG/alley_evening.png"
+      background: "./Images/BG/alley_evening.png"
     },
     nightStreets: {
       name: "Starry",
-      background: "/Template/Images/Backgrounds/nightStreets.jpg"
+      background: "./Images/Backgrounds/nightStreets.jpg"
     }
   };
 
@@ -133,31 +133,31 @@ namespace VisualNovel {
       name: "Jay",
       origin: ƒS.ORIGIN.BOTTOMCENTER,
       pose: {
-        default: "/Template/Images/Characters/Jay/default.png",
-        serious: "/Template/Images/Characters/Jay/serious.png",
-        stressed: "/Template/Images/Characters/Jay/stressed.png",
-        crying: "/Template/Images/Characters/Jay/crying.png",
-        schocked: "/Template/Images/Characters/Jay/schocked.png",
+        default: "./Images/Characters/Jay/default.png",
+        serious: "./Images/Characters/Jay/serious.png",
+        stressed: "./Images/Characters/Jay/stressed.png",
+        crying: "./Images/Characters/Jay/crying.png",
+        schocked: "./Images/Characters/Jay/schocked.png",
       }
     },
     takeshi: {
       name: "Takeshi",
       origin: ƒS.ORIGIN.BOTTOMLEFT,
       pose: {
-        default: "/Template/Images/Characters/Takeshi/default.png",
-        aggresiv: "/Template/Images/Characters/Takeshi/aggresiv.png"
+        default: "./Images/Characters/Takeshi/default.png",
+        aggresiv: "./Images/Characters/Takeshi/aggresiv.png"
       }
     },
     yoshi: {
       name: "Yoshi",
       origin: ƒS.ORIGIN.CENTERLEFT,
-      pose: { default: "/Template/Images/Characters/Yoshi/Default.png" }
+      pose: { default: "./Images/Characters/Yoshi/Default.png" }
     },
     renji: {
       name: "Renji",
 
       origin: ƒS.ORIGIN.BOTTOMRIGHT,
-      pose: { default: "/Template/Images/Characters/Renji/default.png", }
+      pose: { default: "./Images/Characters/Renji/default.png", }
     },
     aika: {
       name: "Aika",
@@ -167,8 +167,8 @@ namespace VisualNovel {
       name: "Ayumi",
       origin: ƒS.ORIGIN.BOTTOMLEFT,
       pose: {
-        angry: "/Template/Images/Characters/Ayumi/sad.png",
-        happy: "/Template/Images/Characters/Ayumi/happy.png",
+        angry: "./Images/Characters/Ayumi/sad.png",
+        happy: "./Images/Characters/Ayumi/happy.png",
       }
     }
   }
